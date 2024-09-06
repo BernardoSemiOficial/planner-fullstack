b5ac86eef90a70536eeed4a06562ba2ef8b7c6ad
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-09-05 21:35:33 -0300

    feat: utilizado as novas rotas

--- journey-backend/src/server.ts
+++ journey-backend/src/server.ts
@@ -7,10 +7,15 @@ import {
 import { confirmParticipant } from "./routes/confirm-participant";
 import { confirmTrip } from "./routes/confirm-trip";
 import { createActivity } from "./routes/create-activity";
+import { createInvite } from "./routes/create-invite";
 import { createLink } from "./routes/create-link";
 import { createTrip } from "./routes/create-trip";
 import { getActivities } from "./routes/get-activities";
 import { getLinks } from "./routes/get-links";
+import { getParticipantDetail } from "./routes/get-participant-detail";
+import { getParticipants } from "./routes/get-participants";
+import { getTripDetail } from "./routes/get-trip-detail";
+import { updateTrip } from "./routes/update-trip";
 const app = fastify();
 const port = 3000;
 
@@ -27,6 +32,11 @@ app.register(createActivity);
 app.register(getActivities);
 app.register(createLink);
 app.register(getLinks);
+app.register(getParticipants);
+app.register(getParticipantDetail);
+app.register(updateTrip);
+app.register(getTripDetail);
+app.register(createInvite);
 
 app.listen({ port }).then(() => {
   console.log(`Server listening at http://localhost:${port}`);
